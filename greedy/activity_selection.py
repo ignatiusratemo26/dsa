40aec04d2c83df800dabfe61bb036acd166e7282
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-10-24 15:17:50 +0300

    initial commit

---
+++ greedy/activity_selection.py
@@ -0,0 +1,5 @@
+# Given n activities with their start and finish times, select the max number of activities that can
+# be performed by a single person assuming that a person can only work on a single activity
+# at a time
+
+def activity_selection():
